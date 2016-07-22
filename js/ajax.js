a0918736e653de8819c5b254507d7f879e4c9d02
Mario Fernando <mariofernando07@hotmail.com>
2016-07-21 19:37:57 -0500

    final

--- js/ajax.js
+++ js/ajax.js
@@ -1,9 +1,8 @@
 var request = new XMLHttpRequest();
-
+var requestUsr = new XMLHttpRequest();
 
 function loadTxt() {
-    alert("entro");
-    request.onreadystatechange = processTxt();//avisa el estado de la peticion ajax. Escucha cada vez que hay un cambio en el request
+    request.onreadystatechange = processTxt;//avisa el estado de la peticion ajax. Escucha cada vez que hay un cambio en el request
     request.open("GET", "data.txt", true);    
     request.send();
 }
@@ -14,4 +13,27 @@ function processTxt() {
         var txt = document.getElementById("txt");
         txt.innerHTML = request.responseText;
     }
+}
+
+//---------------------------------------------------------------------------------------
+function loadUsuario() {
+    requestUsr.onreadystatechange = processUsr;
+    requestUsr.open("GET", "usuarios.json", true);
+    requestUsr.send();
+}
+
+function processUsr() {
+    if(requestUsr.status == 200 && requestUsr.readyState == 4) {
+        var data = requestUsr.responseText;
+        data = JSON.parse(data);
+        var usr = document.getElementById("usr");
+        usr.innerHTML = "";
+        for(i = 0; i < data.length; i++) {
+            var u = data[i];
+            usr.innerHTML += "<h1>" + u.nombre + "</h1>"
+                + "<p>" + u.celular + "</p>"
+                + "<p>" + u.correo + "</p>"
+                + "<hr>";
+        }
+    }    
 }
